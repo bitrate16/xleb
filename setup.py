c74104afde6b8d050bc981bc46c0291c55b83f95
pegasko <pegasko@pegasko.art>
2024-01-05 20:39:55 +0300

    bomp version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xleb',
-    version='1.1',
+    version='1.2',
     py_modules=['xleb'],
     packages=['xleb'],
     description='web-based remote file manager',
